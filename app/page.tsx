006a3047599cf5a7d338c792e3e8e75eb7e3fbc8
yaohenrimichel27-afk <yaohenrimichel27@gmail.com>
2026-06-14 16:22:34 +0000

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>🍽️ La Touche D</h1>
+
+      <a href="/pos">👉 Aller à la caisse</a>
+      <br />
+      <a href="/admin">👉 Dashboard admin</a>
+    </div>
+  );
+}
